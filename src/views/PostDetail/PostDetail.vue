2803cb9660f7c923b318763bbb55504295394187
유진 <yujin.gaya@gmail.com>
2017-09-10 20:07:29 +0900

    Add vuex

--- src/views/PostDetail/PostDetail.vue
+++ src/views/PostDetail/PostDetail.vue
@@ -1,13 +1,24 @@
 <template>
   <div>
     <h1>글 내용</h1>
-    {{post_id}}
+    {{ msg }} <br/>
+    {{ language }}
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
   props: ['post_id'],
+  data() {
+    return {
+      msg: `message ${this.post_id}`,
+    };
+  },
+  computed: {
+    ...mapState(['language']),
+  },
 };
 </script>
 
