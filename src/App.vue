9b0d9fc41aa9fee69617fb255f6206834765b976
유진 <yujin.gaya@gmail.com>
2017-09-14 20:48:42 +0900

    Add bulma and theme color (arbitrary)

--- src/App.vue
+++ src/App.vue
@@ -18,5 +18,12 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+@import './styles/variables.scss';
+
+@import "~bulma/sass/utilities/initial-variables";
+
+$primary: $theme-red;
+
+@import '~bulma';
 </style>
