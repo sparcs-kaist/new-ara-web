677aa2bda56d4619027c57a6788ff0de3446f29d
유진 <yujin.gaya@gmail.com>
2017-10-06 22:24:20 +0900

    Add posting functionality to PostCreate

--- src/store/index.js
+++ src/store/index.js
@@ -6,5 +6,10 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     language: 'ko_KR',
+    apiUrl: 'http://13.124.216.27:8000',
+    auth: {
+      username: 'jsh56son',
+      password: 'asdfasdf1234',
+    },
   },
 });
--- src/views/PostCreate/PostCreate.vue
+++ src/views/PostCreate/PostCreate.vue
@@ -2,21 +2,45 @@
   <div class="container">
     <section class="section">
       <h1 class="title">글쓰기</h1>
-      <vue-editor v-model="content" :editorToolbar="customToolbar" @imageAdded="imageUploadHandler (file, Editor, cursorLocation)" />
-      {{ content }}
-      <button id="button-post-create" class="button is-primary is-pulled-right">게시</button>
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label"> 제목 </label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <input v-model="title" class="input" type="text" placeholder="제목">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label"> 내용 </label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <vue-editor v-model="content" :editorToolbar="customToolbar" @imageAdded="imageUploadHandler (file, Editor, cursorLocation)" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <button :class="{'is-loading': pending}" @click="postArticleHandler" id="button-post-create"class="button is-primary is-pulled-right">게시하기</button>
       <div class="is-clearfix"></div>
     </section>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
 import { VueEditor } from 'vue2-editor';
 
 export default {
   data() {
     return {
-      content: '',
+      title: '',
+      content: ' ',
       customToolbar: [
         ['bold', 'italic', 'underline', 'strike'],
         ['blockquote', 'code-block'],
@@ -25,12 +49,41 @@ export default {
         [{ color: [] }, { background: [] }],
         ['image', 'video'],
       ],
+      pending: false,
     };
+  },
+  computed: {
+    ...mapState(['apiUrl', 'auth']),
   },
   components: {
     VueEditor,
   },
   methods: {
+    postArticleHandler() {
+      this.pending = true;
+      this.$axios({
+        url: `${this.apiUrl}/api/articles/`,
+        method: 'POST',
+        data: {
+          title: this.title,
+          content: this.content,
+          is_anonymous: false,
+          is_content_sexual: true,
+          is_content_social: false,
+          use_signature: false,
+          parent_board: 2,
+        },
+        auth: this.auth,
+      }).then((result) => {
+        this.pending = false;
+        console.log(result);
+      })
+      .catch((err) => {
+        this.pending = false;
+        console.log(err);
+        alert(err);
+      });
+    },
     imageUploadHandler(file, Editor, cursorLocation) {
       const formData = new FormData();
       formData.append('image', file);
@@ -53,8 +106,8 @@ export default {
 </script>
 
 <style>
-#button-post-create {
-  margin-top: 1.5em;
+.is-title {
+  margin-bottom: 2rem;
 }
 
 @media screen and (max-width: 1023px) {
@@ -66,4 +119,9 @@ export default {
     display: none;
   }
 }
+
+#button-post-create {
+  margin-top: 1.5em;
+}
+
 </style>
