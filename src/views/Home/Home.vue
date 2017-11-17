bae3978bd2985c7c18b8c9943289654009d97366
Zeniuus <jsh56son@kaist.ac.kr>
2017-11-18 00:40:42 +0900

    Home.vue: render today best, weekly best articles and articles for each boards

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -6,15 +6,15 @@
     <div class="columns is-multiline">
       <div class="column is-6">
         <h2>오늘 화제</h2>
-        <hit-article v-for="n in 5" :key="n" :title="'글 제목'" :hit="3"/>
+        <hit-article v-for="article in todayBest" :key="article.id" :title="article.title" :hit="article.hit_count"></hit-article>
       </div>
       <div class="column is-6">
         <h2>이번주 화제</h2>
-        <hit-article v-for="n in 5" :key="n" :title="'글 제목'" :hit="3"/>
+        <hit-article v-for="article in weeklyBest" :key="article.id" :title="article.title" :hit="article.hit_count"></hit-article>
       </div>
-      <div class="column is-6" v-for="board in ['PLAY', 'TALK', 'LOVE', 'SECRET', 'WANTED', 'BUY SELL']" :key="board">
-        <h2>{{ board }}</h2>
-        <recent-article v-for="n in 5" :key="n" :title="'글 제목'" :time="'3시간 전'" :no-comments="2"/>
+      <div class="column is-6" v-for="boardName in boardNameList" :key="boardName">
+        <h2>{{ boardName }}</h2>
+        <recent-article v-for="article in articles[boardName]" :key="article.id" :title="article.title" :time="'3시간 전'" :no-comments="2"></recent-article>
       </div>
     </div>
   </div>
