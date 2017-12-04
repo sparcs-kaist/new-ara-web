96ae6c81e11aee69809126b9e0fbbed159082fe6
Zeniuus <jsh56son@kaist.ac.kr>
2017-12-04 17:12:34 +0900

    [Footer] Apply CSS for responsive design

--- src/components/Foot/Foot.vue
+++ src/components/Foot/Foot.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="container">
-    <footer>
-      <p class="content text-has-centered">
-        아라 by <a href="https://sparcs.org">SPARCS</a><br>
-        since 2017
-      </p>
-    </footer>
+    <div class="columns">
+      <div class="column is-2 placeholder"></div>
+      <div class="column is-8">
+        <footer>
+          <hr>
+          <p class="content text-has-centered">
+            아라 by <a href="https://sparcs.org">SPARCS</a><br>
+            since 2017
+          </p>
+        </footer>
+      </div>
+      <div class="column is-2 placeholder"></div>
+    </div>
   </div>
 </template>
 
@@ -16,7 +23,24 @@ export default {
 </script>
 
 <style scoped>
-footer {
-  padding: 4rem 0;
-}
+  footer {
+    text-align: center;
+    font-size: 14px;
+  }
+
+  @media (min-width: 769px) {
+    footer {
+      padding: 2rem 0;
+    }
+  }
+
+  @media (max-width: 768px) {
+    footer {
+      padding: 2rem 10px;
+    }
+
+    .placeholder {
+      padding: 0;
+    }
+  }
 </style>
