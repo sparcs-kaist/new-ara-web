ce889eea4c7836de47622b584ab5ac472be21994
Yujin Kim <yujingaya@Yujins-MacBook-Pro.local>
2018-03-25 21:47:29 +0900

    Use timeagojs

--- src/views/PostList/PostItem.vue
+++ src/views/PostList/PostItem.vue
@@ -1,20 +1,24 @@
 <template>
   <tr>
-    <td class=" w-35"></td>
-    <td class=" w-102">{{ heading }}</td>
-    <td class=" w-313">
+    <td class="w-35"></td>
+    <td class="w-102">{{ heading }}</td>
+    <td class="w-313">
       <router-link v-if="!post || item.id !== post.id" :to="{ name: 'PostDetail', params: { board, post_id: item.id }, query: $route.query }">
         {{ item.title }}
       </router-link>
     </td>
-    <td class=" w-102">{{ item.created_by }}</td>
-    <td class=" w-50">{{ item.positive_vote_count }}/{{ item.negative_vote_count }}</td>
-    <td class=" w-50">{{ item.hit_count }}</td>
-    <td class=" w-102">{{ item.created_at }}</td>
+    <td class="w-102">{{ item.created_by }}</td>
+    <td class="w-50">{{ item.positive_vote_count }}/{{ item.negative_vote_count }}</td>
+    <td class="w-50">{{ item.hit_count }}</td>
+    <td class="w-102">{{ readable_timeago }}</td>
   </tr>
 </template>
 
 <script>
+import timeago from 'timeago.js';
+import ko from 'timeago.js/locales/ko';
+
+timeago.register('ko', ko);
 
 export default {
   props: ['board', 'item', 'post'],
@@ -23,6 +27,10 @@ export default {
       if (this.board === 'all') return this.item.parent_board.en_name;
       if (this.item.parent_topic !== null) return this.item.parent_topic.en_name;
       return '';
+    },
+    readable_timeago() {
+      const timestamp = Date.parse(this.item.created_at);
+      return timeago().format(timestamp, 'ko');
     },
   },
 };
