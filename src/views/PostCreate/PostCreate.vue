9c2480598aa60180ebc03055496624d9b48d680c
Zeniuus <jsh56son@kaist.ac.kr>
2018-06-03 16:37:49 +0900

    Fix bug: redirect to correct url after creating an article

--- src/views/PostCreate/PostCreate.vue
+++ src/views/PostCreate/PostCreate.vue
@@ -97,8 +97,8 @@ export default {
         },
       }).then((res) => {
         this.pending = false;
-        this.$router.push(`/posts/${res.data.parent_board.id > 0
-          ? this.boardNameList[res.data.parent_board.id - 1] : 'all'}/${res.data.id}`);
+        this.$router.push(`/posts/${res.data.parent_board > 0
+          ? this.boardNameList[res.data.parent_board - 1] : 'all'}/${res.data.id}`);
       })
       .catch(() => {
         this.pending = false;
