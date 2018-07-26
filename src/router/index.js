389273134560f14757f778182ff94f2d78504ed9
Yujin Kim <yujinkim@Yujins-MacBook-Pro.local>
2018-07-26 14:53:25 +0900

    Add 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import auth from './auth.js'
 import app from './app.js'
+import NotFound from '@/views/NotFound.vue'
 
 Vue.use(Router)
 
@@ -9,7 +10,12 @@ export default new Router({
   mode: 'history',
   routes: [
     ...auth,
-    ...app
+    ...app,
+    {
+      path: '*',
+      name: 'not-found',
+      component: NotFound
+    }
   ],
   scrollBehavior (to, from, savedPosition) {
     if (savedPosition) return savedPosition
---
+++ src/views/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    페이지를 찾을 수 없습니다.
+    <router-link
+      :to="{ name: 'home' }">
+      홈으로
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'not-found'
+}
+</script>
+
+<style>
+
+</style>
