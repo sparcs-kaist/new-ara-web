9eb57b1936e646cb80905ce01e014023676458c7
YujinGaya <yujin.gaya@gmail.com>
2018-08-07 03:00:18 +0900

    Add notice on advertisements

--- src/components/TheAd.vue
+++ src/components/TheAd.vue
@@ -1,6 +1,11 @@
 <template>
-  <div id="side-ad">
-    광고
+  <div class="ad-container">
+    <div id="side-ad">
+      광고
+    </div>
+    <div class="about-ad">
+      아라에 광고를 달고 싶다면 관리자에게 문의해주세요.
+    </div>
   </div>
 </template>
 
@@ -11,17 +16,27 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-#side-ad {
-  // 사이즈는 Google AdSense를 참고함
-  // @ref: https://support.google.com/adsense/answer/6002621?hl=en
-  height: 600px;
+.ad-container {
   width: 160px;
 
-  // for demonstration only
-  background-color: whitesmoke;
-  text-align: center;
-  line-height: 600px;
-  color: silver;
-  font-weight: 800;
+  #side-ad {
+    // 사이즈는 Google AdSense를 참고함
+    // @ref: https://support.google.com/adsense/answer/6002621?hl=en
+    height: 600px;
+    width: 160px;
+    position: relative;
+
+    // for demonstration only
+    background-color: whitesmoke;
+    text-align: center;
+    line-height: 600px;
+    color: silver;
+    font-weight: 800;
+  }
+
+  .about-ad {
+    font-size: 0.75rem;
+    padding: 0.5rem 0;
+  }
 }
 </style>
