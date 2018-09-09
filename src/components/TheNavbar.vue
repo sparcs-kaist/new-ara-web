3a996bdce049b7be77653bca9876983718967b0e
YujinGaya <yujin.gaya@gmail.com>
2018-09-09 19:33:57 +0900

    Add dropdown to select from multiple languages (in the future hopefully)

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -50,15 +50,24 @@
           class="navbar-item">
           {{ $t('notification') }}
         </router-link>
-        <a
-          class="navbar-item"
-          @click="$i18n.locale = $i18n.locale === 'en' ? 'ko' : 'en'">
-          언어 변경
-        </a>
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link is-arrowless">
+            <img :src="require('@/assets/language.svg')"/>
+          </a>
+          <div class="navbar-dropdown is-boxed is-right">
+            <a class="navbar-item"
+              @click="$i18n.locale = $i18n.locale === 'en' ? 'ko' : 'en'">
+              {{ $i18n.locale === 'en' ? '한국어' : 'English' }}
+            </a>
+          </div>
+        </div>
         <router-link
           :to="{ name: 'settings' }"
           class="navbar-item">
-          {{ userNickname }}님
+          <img
+            :src="require('@/assets/user.svg')"
+            class="user-svg"/>
+          {{ userNickname }}
         </router-link>
       </div>
     </div>
@@ -102,27 +111,34 @@ export default {
 .navbar {
   border-top: 5px solid $theme-red;
   margin-bottom: 1.5rem;
-}
 
-// @TODO: Is ther a better way of achieving this..?
-.navbar-brand {
-  @media (min-width: 1088px) {
-    width: calc(calc(100% - 960px) / 2);
+  // @TODO: Is ther a better way of achieving this..?
+  .navbar-brand {
+    @media (min-width: 1088px) {
+      width: calc(calc(100% - 960px) / 2);
+    }
+    @media (min-width: 1280px) {
+      width: calc(calc(100% - 1152px) / 2);
+    }
+    @media (min-width: 1472px) {
+      width: calc(calc(100% - 1344px) / 2);
+    }
+    a:hover {
+      text-decoration: none;
+    }
   }
-  @media (min-width: 1280px) {
-    width: calc(calc(100% - 1152px) / 2);
-  }
-  @media (min-width: 1472px) {
-    width: calc(calc(100% - 1344px) / 2);
-  }
-  a:hover {
-    text-decoration: none;
-  }
-}
 
-// 왼쪽 음수 여백으로 줄맞춤
-.navbar-menu {
-  margin: 0 0.75rem 0 -0.75rem;
+  // 왼쪽 음수 여백으로 줄맞춤
+  .navbar-menu {
+    margin: 0 0.75rem 0 -0.75rem;
+  }
+
+  .navbar-link.is-arrowless {
+    padding-right: 0.75rem;
+    &::after {
+      content: none;
+    }
+  }
 }
 
 .notification.is-api-error {
@@ -132,5 +148,9 @@ export default {
   right: 0;
   margin: 0 auto;
   width: 40em;
+}
+
+.user-svg {
+  margin-right: 0.5rem;
 }
 </style>
