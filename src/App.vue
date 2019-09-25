73eaaff601806dcb8b2bdeebf570a82a25dddd88
Jinwoo Jeong <thinkratomos@gmail.com>
2019-09-26 00:12:30 +0900

    Make footer sticky.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div id="root-container">
+    <div id="app">
+      <router-view/>
+    </div>
+    <TheFooter />
   </div>
 </template>
 
 <script>
+import TheFooter from '@/components/TheFooter.vue'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: { TheFooter }
 }
 </script>
 
 <style lang="scss">
 @import '@/bulma/index.scss';
+#root-container {
+  #app {
+    @media screen and (min-width: 700px) {
+      min-height: 100vh;
+      padding-bottom: 54px;
+      margin-bottom: -54px;
+    }
+    // background-color: gray;
+  }
+}
 </style>
