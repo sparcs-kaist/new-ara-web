df871bcad52a322089a641342a9ee652cc5549f9
Khinenw <deu07115@gmail.com>
2020-10-05 23:35:57 +0900

    Show new on unread post

--- src/components/BoardItem.vue
+++ src/components/BoardItem.vue
@@ -27,6 +27,10 @@
 
             {{ post.title }}
           </div>
+
+          <div class="board-item__new" v-if="post.read_status === 'N'">
+            {{ $t('new') }}
+          </div>
         </div>
       </div>
     </div>
@@ -89,10 +93,12 @@ export default {
 ko:
   comments: '댓글'
   views: '조회수'
+  new: 'new'
 
 en:
   comments: 'Reply'
   views: 'View'
+  new: 'new'
 </i18n>
 
 <style lang="scss" scoped>
@@ -121,12 +127,15 @@ en:
     width: 40px;
     height: 40px;
     border-radius: 50%;
+    flex: 0 0 auto;
 
     margin-right: 20px;
   }
 
   &__content {
     flex: 1;
+    width: 0;
+    margin-right: 10px;
     display: flex;
     flex-direction: column;
     align-items: stretch;
@@ -144,13 +153,13 @@ en:
 
   &__title-wrapper {
     display: flex;
+    align-items: center;
   }
 
   &__title {
     font-size: 1rem;
     font-weight: 500;
-    flex: 1 1 0;
-    width: 0;
+    flex: 0 1 auto;
     overflow: hidden;
     text-overflow: ellipsis;
     white-space: nowrap;
@@ -159,6 +168,13 @@ en:
   &__topic {
     color: var(--grey-400);
     font-weight: 400;
+  }
+
+  &__new {
+    color: var(--theme-400);
+    font-size: .8rem;
+    font-weight: 500;
+    margin-left: 5px;
   }
 
   &:first-child {
