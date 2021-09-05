646bdc0895d5551ac6389f050c5a19fca063aab6
idev2580 <jic4656@naver.com>
2021-09-05 17:16:23 +0900

    Fix autofill category to fill when there is no category in list.

--- src/components/ThePostWrite.vue
+++ src/components/ThePostWrite.vue
@@ -294,7 +294,11 @@ export default {
 
   watch: {
     boardId () {
-      this.categoryId = ''
+      if (this.categoryList.length) {
+        this.categoryId = '$not-set'
+      } else {
+        this.categoryId = ''
+      }
     }
   },
 
