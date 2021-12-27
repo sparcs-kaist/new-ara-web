9f1f7eb3a4f290bc6994c29808cac425c5cc232b
username <hui051875@gmail.com>
2021-12-27 21:58:27 +0900

    add block rate limit

--- src/api/user.js
+++ src/api/user.js
@@ -46,7 +46,6 @@ export const fetchBlocks = () =>
 
 export const deleteBlock = (blockId) =>
   http.delete(`blocks/${blockId}/`)
-    .then(({ data }) => data)
 
 export const updateDarkMode = (userId, darkMode) =>
   http.patch(`user_profiles/${userId}/`, { extra_preferences: { darkMode } })
