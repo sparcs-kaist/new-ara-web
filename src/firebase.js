d20f7ea19e89f33e647f010b4d4992919252598e
Triangleyj <dpwns99@gmail.com>
2022-11-16 21:12:52 +0900

    Detach onMessage from acquireFCMToken, add toast

--- src/firebase.js
+++ src/firebase.js
@@ -12,47 +12,50 @@ const app = firebase.initializeApp(firebaseConfig)
 let acquired = false
 
 export const acquireFCMToken = async () => {
-  if ('Notification' in window) {
+  if (!acquired && 'Notification' in window) {
     Notification.requestPermission().then(function (result) {
       if (result === 'granted') {
-        const messaging = getMessaging(app)
-        if (!acquired) {
-          getToken(messaging, { vapidKey: process.env.VUE_APP_FIREBASE_VAPID_KEY })
-            .then((currentToken) => {
-              if (currentToken) {
-                // Send the token to your server and update the UI if necessary
-                // console.log('[Notification] client token', currentToken)
-                updateFCMToken(currentToken)
-                acquired = true // prevent from acquiring token again after first access. If the page is reloaded, the token will be acquired again.
-              } else {
-                // Show permission request UI
-                console.log('[Notification] No registration token available. Request permission to generate one.')
-              }
-            }).catch((err) => {
-              console.log('[Notification] An error occurred while retrieving token. ', err)
-            })
-        }
-        // Foreground notification
-        onMessage(messaging, (payload) => {
-          navigator.serviceWorker.getRegistration('/firebase-cloud-messaging-push-scope').then(registration => {
-            registration.showNotification(
-              payload.notification.title,
-              {
-                body: payload.notification.body, // Maybe bug: no enough information of WebpushNotification on payload.notification
-                icon: '/img/icons/ara-pwa-192.png',
-                tag: 'foreground-notification',
-                action_open_url: payload.data.action_open_url,
-                renotify: true,
-                data: payload.data
-              }
-            )
+        getToken(getMessaging(app), { vapidKey: process.env.VUE_APP_FIREBASE_VAPID_KEY })
+          .then((currentToken) => {
+            if (currentToken) {
+              // Send the token to your server and update the UI if necessary
+              // console.log('[Notification] client token', currentToken)
+              updateFCMToken(currentToken)
+              acquired = true // prevent from acquiring token again after first access. If the page is reloaded, the token will be acquired again.
+            } else {
+              // Show permission request UI
+              console.log('[Notification] No registration token available. Request permission to generate one.')
+            }
+          }).catch((err) => {
+            console.log('[Notification] An error occurred while retrieving token. ', err)
           })
-        })
       } else {
         alert('권한 허용을 해 주셔야 PUSH 알림을 받으실 수 있습니다.')
+        acquired = true
       }
     })
   }
+}
+
+export const onMessageListener = (context) => {
+  // Foreground notification
+  onMessage(getMessaging(app), (payload) => {
+    navigator.serviceWorker.getRegistration('/firebase-cloud-messaging-push-scope').then(async registration => {
+      await context.reloadNotification()
+      context.$store.dispatch('dialog/toast', payload.notification.title + '\n' + payload.notification.body)
+      // registration.showNotification(
+      //   payload.notification.title,
+      //   {
+      //     body: payload.notification.body, // Maybe bug: no enough information of WebpushNotification on payload.notification
+      //     icon: '/img/icons/ara-pwa-192.png',
+      //     tag: 'foreground-notification',
+      //     action_open_url: payload.data.action_open_url,
+      //     renotify: true,
+      //     data: payload.data
+      //   }
+      // )
+    })
+  })
 }
 
 export const releaseFCMToken = async () => {
@@ -66,6 +69,6 @@ export const releaseFCMToken = async () => {
         // Nothing to do with
       }
     }).catch((err) => {
-      console.log('[Notification] An error occurred while retrieving token. ', err)
+      console.log('[Notification] An error occurred while releasing token. ', err)
     })
 }
