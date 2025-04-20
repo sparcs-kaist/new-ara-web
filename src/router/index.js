518e7156e8c25e774f465d85d0ad63996acc23ac
SeungBeom-Park <127308007+sjsh16kw@users.noreply.github.com>
2025-04-21 06:24:54 +0900

    Try4 : 라우터 가드 추가
    빌드 오류 해결

--- src/router/index.js
+++ src/router/index.js
@@ -34,7 +34,7 @@ const router = new Router({
 
 // 라우터 가드 설정
 router.beforeEach((to, from, next) => {
-  const userAgreed = store.state.auth?.userProfile?.agree_terms_of_service_at;
+  const userAgreed = store.state.auth?.userProfile?.agree_terms_of_service_at
 
   // 약관에 동의하지 않은 경우 약관 페이지로 리다이렉트
   if (!userAgreed && to.path !== '/terms') {
